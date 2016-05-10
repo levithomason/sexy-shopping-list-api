17433ddc6f79bf306a86a3edf7d2a56a134fa68d
levithomason <me@levithomason.com>
2016-05-09 19:33:51 -0700

    finish generic endpoints

--- src/main.js
+++ src/main.js
@@ -10,54 +10,47 @@ app
   .use(bodyParser.json())
 
   .get('/:collection', (req, res) => {
-    const objects = storage.list(req.params.collection)
+    const { collection } = req.params
+    const objects = storage.list(collection)
 
     if (objects) {
       res.json(objects)
     } else {
-      res.status(404).send(`Could not find collection: ${req.params.collection}`)
+      res
+        .status(404)
+        .send(`Could not find collection: ${collection}`)
     }
   })
 
   .get('/:collection/:id', (req, res) => {
     const { collection, id } = req.params
-
     const object = storage.find(collection, id)
 
     if (object) {
       res.json(object)
     } else {
-      res.status(404).send(`Could not find object with id "${id}" in collection "${collection}".`)
+      res
+        .status(404)
+        .send(`Could not find object with id "${id}" in collection "${collection}".`)
     }
   })
 
-  .post('/categories', (req, res) => {
-    const saved = storage.add('categories', req.body)
+  .post('/:collection', (req, res) => {
+    const { collection } = req.params
+    // TODO storage should gracefully handle non-existent collections
+    const saved = storage.add(collection, req.body)
     res.json(saved)
   })
 
-  .delete('/categories/:id', (req, res) => {
-    const wasFound = storage.remove('categories', req.params.id)
-
-    const message = wasFound
-      ? `Successfully removed category id: ${req.params.id}`
-      : `Did not find category id: ${req.params.id}`
-
-    res.status(404).send(message)
-  })
-
-  .post('/items', (req, res) => {
-    const saved = storage.add('items', req.body)
-    res.json(saved)
-  })
-
-  .delete('/items/:id', (req, res) => {
-    const wasFound = storage.remove('items', req.params.id)
-
+  .delete('/:collection/:id', (req, res) => {
+    const { collection, id } = req.params
+    const wasFound = storage.remove(collection, id)
     if (wasFound) {
-      res.send(`Successfully removed item id: ${req.params.id}`)
+      res.send(`Successfully removed ${collection} id: ${id}`)
     } else {
-      res.status(404).send(`Did not find item id: ${req.params.id}`)
+      res
+        .status(404)
+        .send(`Did not find ${collection} id: ${id}`)
     }
   })
 
